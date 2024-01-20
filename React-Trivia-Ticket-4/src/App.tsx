f8c664803e64eb521a47cb68faa94a4fd7875a9e
jesdone <95374712+jesdone@users.noreply.github.com>
2024-01-20 10:52:12 -0500

    Four Catagory Cards Added with Images

--- React-Trivia-Ticket-4/src/App.tsx
+++ React-Trivia-Ticket-4/src/App.tsx
@@ -8,8 +8,6 @@ function App() {
     <>
     <div>
       <Heading />
-    </div>
-    <div>
       <Catagories/>
     </div>
     </>
--- React-Trivia-Ticket-4/src/Catagories.tsx
+++ React-Trivia-Ticket-4/src/Catagories.tsx
@@ -3,48 +3,75 @@
 const Catagories = () => {
   return (
     <>
-    <div className="card width: 2 rem;"> 
-      <img
-        src="https://media.timeout.com/images/105795964/750/422/image.jpg"
-        className="card-img-top"
-        alt="..."
-      />
-      <div className="card-body">
-        <h5 className="card-title">Paintings</h5>
-        <p className="card-text">
-          Art trivia questions from the Paintings Catagory.
-        </p>
-        <a href="#" className="btn btn-primary">
-          Go To Questions
-        </a>
+      <div className="card width: 2 rem;">
+        <img
+          src="https://media.timeout.com/images/105795964/750/422/image.jpg"
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">Painting</h5>
+          <p className="card-text">
+            Art trivia questions from the Painting Catagory.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Go To Questions
+          </a>
+        </div>
       </div>
-    </div>
 
+      <div className="card width: 2 rem;">
+        <img
+          src="https://mymodernmet.com/wp/wp-content/uploads/2017/08/Famous-Sculptures-Louise-Bourgeois.jpg"
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">Sculpture</h5>
+          <p className="card-text">
+            Art trivia questions from the Sculpture Catagory.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Go To Questions
+          </a>
+        </div>
+      </div>
 
+      <div className="card width: 2 rem;">
+        <img
+          src="https://architizer-prod.imgix.net/media/14666943648613052694-inline-gaudi-basilica.jpg?fit=max&w=1680&q=60&auto=format&auto=compress&cs=strip"
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">Architecture</h5>
+          <p className="card-text">
+            Art trivia questions from the Architecture Catagory.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Go To Questions
+          </a>
+        </div>
+      </div>
 
-<div className="card width: 2 rem;"> 
-<img
-  src="https://mymodernmet.com/wp/wp-content/uploads/2017/08/Famous-Sculptures-Louise-Bourgeois.jpg"
-  className="card-img-top"
-  alt="..."
-/>
-<div className="card-body">
-  <h5 className="card-title">Sculptures</h5>
-  <p className="card-text">
-    Art trivia questions from the Sculptures Catagory.
-  </p>
-  <a href="#" className="btn btn-primary">
-    Go To Questions
-  </a>
-</div>
-</div>
-
-
-</>
+      <div className="card width: 2 rem;">
+        <img
+          src="https://art2arts.co.uk/magazine/wp-content/uploads/2020/03/file_1_44.jpg"
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">Literature</h5>
+          <p className="card-text">
+            Art trivia questions from the Literature Catagory.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Go To Questions
+          </a>
+        </div>
+      </div>
+    </>
   );
 };
-
-
-
 
 export default Catagories;
